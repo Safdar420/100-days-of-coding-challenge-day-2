cd4e7204ba68bd8e1817ffa96784e399f2c29d54
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-13 20:29:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//  Q no 1
+var FamousQuote1 = "A person who never made a mistake never tried anything new";
+console.log("Albert Einstein once said,\"".concat(FamousQuote1, "\""));
+// Q no 2
+var FamousPerson = "Albert Einstein";
+var FamousPerson2 = "A person who never made a mistake never tried anything new";
+var Message = "".concat(FamousPerson, " once said,\"").concat(FamousPerson2, "\" ");
+console.log(Message);
+// Q no 3
+var PersonNameWithWhitespace = " \t  Safdar Ali  \n";
+console.log(PersonNameWithWhitespace);
+var StrippedName = PersonNameWithWhitespace.trim();
+console.log(StrippedName);
---
+++ main.ts
@@ -0,0 +1,17 @@
+//  Q no 1
+let FamousQuote1 : string ="A person who never made a mistake never tried anything new";
+console.log(`Albert Einstein once said,"${FamousQuote1}"`);
+
+// Q no 2
+
+let FamousPerson :string ="Albert Einstein";
+let FamousPerson2 :string ="A person who never made a mistake never tried anything new";
+let Message :string =`${FamousPerson} once said,"${FamousPerson2}" `;
+console.log(Message);
+
+// Q no 3
+
+let PersonNameWithWhitespace :string =" \t  Safdar Ali  \n";
+console.log(PersonNameWithWhitespace);
+let StrippedName  =PersonNameWithWhitespace.trim();
+console.log(StrippedName);
